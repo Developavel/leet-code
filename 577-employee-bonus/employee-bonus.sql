d3a77fa6621b973f1c3ab7622b33e36565eb9167
Pavel M <106969593+Developavel@users.noreply.github.com>
2025-11-11 22:27:56 +0300

    Time: 966 ms (38.36%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,4 +1,4 @@
-SELECT name, bonus
-FROM Employee
-LEFT JOIN Bonus ON Employee.empId = Bonus.empId
-WHERE COALESCE(bonus, 0) < 1000;
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
